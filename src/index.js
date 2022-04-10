6c84758780f7ac633971c8050a8a7909638b8b18
Islam Kortam <eng.islam.kortam@gmail.com>
2022-04-10 09:55:59 +0200

    removing unnecessary comments

--- src/index.js
+++ src/index.js
@@ -6,19 +6,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
 
 
-import TodoItem from './components/TodoItem';
-import TodoInput from './components/TodoInput';
 import TodoApp from './components/TodoApp';
 
 
 const localStorageKey = "my-todo-app-imkortam"
 
-const onToggleCheckHandler = (item)=> {
-    const newCompletedState = ! this.state.item.completed
-    let newItem = {...this.props.item}
-    newItem.completed = newCompletedState
-    this.setState({item: newItem})
-}
 
 
 let items = JSON.parse(localStorage.getItem(localStorageKey));
